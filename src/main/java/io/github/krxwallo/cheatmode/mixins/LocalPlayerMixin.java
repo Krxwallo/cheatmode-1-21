0bff05ad0a060333d914fe3bc588bbccac4ca955
Krxwallo <59575572+Krxwallo@users.noreply.github.com>
2024-07-15 18:46:18 +0200

    fix flying, ver 3.0.0-beta

--- src/main/java/io/github/krxwallo/cheatmode/mixins/LocalPlayerMixin.java
+++ src/main/java/io/github/krxwallo/cheatmode/mixins/LocalPlayerMixin.java
@@ -3,7 +3,6 @@ package io.github.krxwallo.cheatmode.mixins;
 import io.github.krxwallo.cheatmode.config.Config;
 import io.github.krxwallo.cheatmode.events.Hooks;
 import net.minecraft.client.player.LocalPlayer;
-import net.minecraft.world.entity.player.Abilities;
 import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,8 +10,8 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(LocalPlayer.class)
 public class LocalPlayerMixin {
-    @Redirect(at = @At(value = "FIELD", target = "Lnet/minecraft/world/entity/player/Abilities;flying:Z", opcode = Opcodes.GETFIELD), method = "aiStep")
-    public boolean mayfly(Abilities instance) {
+    @Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/client/player/LocalPlayer;mayFly()Z", opcode = Opcodes.GETFIELD), method = "aiStep")
+    public boolean mayfly(LocalPlayer instance) {
         if (Hooks.mc().gameMode == null) return false;
         return Hooks.mc().gameMode.getPlayerMode().isCreative() || Config.CLIENT.flight.get();
     }
